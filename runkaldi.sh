ea9bc94355c87e0d3ceacc0416e6a55b79543cb1
5roop <rupnikpeter@gmail.com>
2024-10-03 14:11:21 +0200

    Add debug scripts and files

--- runkaldi.sh
+++ runkaldi.sh
@@ -1,8 +1,14 @@
 
-wavscp=tmp/MP_21_369.29-402.24.wav.scp
-spk2utt=tmp/MP_21_369.29-402.24.spk2utt
-text=tmp/MP_21_369.29-402.24.text
-tmpdir=brisi/
+tmpdir=kaldidebug/
+mkdir -p "$tmpdir"
+echo "speaker1 sampleid">kaldidebug/spk2utt
+echo "sampleid data/MPmp3_wav/MP_27_121.62-132.06.wav" > kaldidebug/wavscp
+echo "sampleid jenu večer je pozabiv stakleno zvono ili je morda ovca po noći tiho odašla ta put se si zvončići store va suze to je kako bi reć vela tajna " > kaldidebug/text
+
+
+wavscp=kaldidebug/wavscp
+spk2utt=kaldidebug/spk2utt
+text=kaldidebug/text
 
 models=models
 kaldi=/opt/kaldi
@@ -22,7 +28,7 @@ $kaldi/src/bin/compile-train-graphs --read-disambig-syms=$tmpdir/disambig.int $m
 $kaldi/src/nnet3bin/nnet3-latgen-faster --allow_partial=True --online-ivectors=ark:$tmpdir/ivec.ark --online-ivector-period=10 $models/nnet3/tdnn1a_sp/final.mdl ark:$tmpdir/graphs.fsts ark:$tmpdir/mfcc.ark ark:$tmpdir/ali.lat
 $kaldi/src/latbin/lattice-align-words $tmpdir/word_boundary.int $models/nnet3/tdnn1a_sp/final.mdl ark:$tmpdir/ali.lat ark:- | $kaldi/src/latbin/lattice-to-ctm-conf ark:- - | $kaldi/egs/wsj/s5/utils/int2sym.pl -f 5 $tmpdir/words.txt - > $tmpdir/ali.ctm
 
-# # # My additions:
+# # # Phoneme alignment:
 $kaldi/src/latbin/lattice-align-phones --replace-output-symbols=true $models/nnet3/tdnn1a_sp/final.mdl ark:$tmpdir/ali.lat ark:$tmpdir/phone_aligned.lats
 $kaldi/src/latbin/lattice-to-ctm-conf --inv-acoustic-scale=10 --decode-mbr ark:$tmpdir/phone_aligned.lats $tmpdir/trans_phones.ctm
 $kaldi/egs/wsj/s5/utils/int2sym.pl -f 5 $tmpdir/phones.txt $tmpdir/trans_phones.ctm > $tmpdir/trans_phones_with_symbols.ctm
